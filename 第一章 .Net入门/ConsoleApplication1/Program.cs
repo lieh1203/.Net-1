b37c7e83096173a98a06b464b7b3b2d426c3c8fd
LAPTOP-3HBRAV9M\李恩华 <lieh1203@163.com>
2017-12-13 23:14:46 +0800

    加2

--- 第一章 .Net入门/ConsoleApplication1/Program.cs
+++ 第一章 .Net入门/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace 控制台编程1
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
-            //让计算机发出蜂鸣声1
+            //让计算机发出蜂鸣声2
             Console.Beep(1000, 1000);
             Console.Clear(); //清除屏幕内容
             string s = Console.ReadLine();
