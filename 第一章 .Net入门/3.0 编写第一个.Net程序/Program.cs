4202d7be406c831a266aaf7f774d9e9757105d03
LAPTOP-3HBRAV9M\李恩华 <lieh1203@163.com>
2017-12-13 22:59:22 +0800

    把123改成123456

--- 第一章 .Net入门/3.0 编写第一个.Net程序/Program.cs
+++ 第一章 .Net入门/3.0 编写第一个.Net程序/Program.cs
@@ -10,7 +10,7 @@ namespace _3._0_编写第一个.Net程序
     {
         static void Main(string[] args)
         {
-            //123
+            //123456
             Console.WriteLine("hello world");
             Console.ReadKey();
         }
